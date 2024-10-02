be9b13596ea09713f785daa81b0438f51c077eb3
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2024-10-02 21:53:30 +0300

    03_lesson-03_after_check

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,11 +1,11 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(numberOne, numberTwo, numberThree) { 
-    if (numberOne > numberTwo && numberOne > numberThree){ 
+    if (numberOne >= numberTwo && numberOne >= numberThree){ 
     return numberOne 
-    } if (numberTwo > numberOne && numberTwo > numberThree){ 
+    } if (numberTwo >= numberOne && numberTwo >= numberThree){ 
     return numberTwo 
-    } if (numberThree > numberOne && numberThree > numberTwo){ 
+    } if (numberThree >= numberOne && numberThree >= numberTwo){ 
     return numberThree 
     } 
 }
